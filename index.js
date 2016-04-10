cb49b286e6909b65fa5a73060f1e72ed163a24ff
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-04-10 15:53:15 -0700

    added index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-'use strict';
+// 'use strict';
 const fs = require('fs');
 // let bitmap = fs.readFileSync(__dirname + '/duck.bmp');
-let bitmap = fs.readFileSync(__dirname + '/' + process.argv[2]);
+var bitmap = fs.readFileSync(__dirname + '/' + process.argv[2]);
 
 
-let bitmapData = {};
+var bitmapData = {};
 bitmapData.headField = bitmap.toString('ascii',0,2);
 bitmapData.size = bitmap.readUInt32LE(2);
 bitmapData.pixelArrayStart = bitmap.readUInt32LE(10);
 bitmapData.paletteColors = bitmap.readUInt32LE(46);
-bitmapData.thing = 'yes';
 bitmapData.compressionMethod = bitmap.readUInt32LE(30);
 bitmapData.maybeColors = bitmap.readUInt32LE(46);
 bitmapData.height = bitmap.readUInt32LE(22);
@@ -19,7 +18,52 @@ bitmapData.theSize= bitmap.readUInt32LE(34);
 bitmapData.vip = bitmap.readUInt32LE(54);
 
 console.dir(bitmapData);
-console.log(bitmap[1078]);
-console.log(bitmap[1080]);
+console.log(bitmap.readUInt32LE(2));
+var colors ={};
+colors.getTheColors = function(){
 
-// fs.writeFileSync('x.bmp', bitmap);
+  for (var i = 54; i<64; i++){
+    console.log('colors: ' + bitmap[i]);
+  };
+  // console.log('Color: ' + bitmap[54]);
+};
+
+
+colors.alterColors = function(){
+  var x = bitmap[58];
+    // var x = bitmap[90];
+  for (var i = 55; i<1078; i = i+2){
+    bitmap[i] = x;
+  };
+
+  return bitmap;
+};
+
+colors.getTheColors();
+// colors.alterColors();
+
+colors.changeTheColors = function(){
+// 90 = green streak; // 54 = blue;// 55 = green; // 56 =red;
+
+  for(var i = 55;i<1078; i++){
+    // if i =
+    // bitmap.writeUInt32LE(255, 54);
+    bitmap.writeUInt32LE(255, i);
+    // bitmap.writeUInt32LE(0, i+1);
+    // bitmap.writeUInt32LE(0, i+2);
+
+    console.log('break');
+    // colors.getTheColors();
+    colors.alterColors();
+    colors.getTheColors();
+    return(bitmap);
+  };
+
+};
+
+colors.changeTheColors();
+
+fs.writeFile('newimageXX.bmp', bitmap, (err)=>{
+  if (err) throw err;
+  console.log('done');
+});
