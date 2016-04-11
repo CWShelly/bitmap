b621dd73b9e4223d2349d512963bfada76ae1617
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-04-11 12:31:52 -0700

    adding files

--- test/image_test.js
+++ test/image_test.js
@@ -1,16 +1,9 @@
 const expect = require('chai').expect;
-const bmt = require(__dirname + '../../lib/change.bmp');
-
-// describe('size of the file', ()=>{
-//   beforeEach(()=>{
-//     // this.file = __dirname + '../palette.bmp'
-//   })
-// })
-
+const bmt = require(__dirname + '../../lib/change.js');
 
 describe('size of the file',()=>{
   beforeEach(()=>{
-    this.file = __dirame bmp
-  })
-  it('should equal the size of the newly created file');
-})
+    this.file = palette.bmp;
+    it('should equal the size of the newly created file');
+  });
+});
