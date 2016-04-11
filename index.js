deff8740001e4927e703134fcebb551e943345b4
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-04-10 18:04:40 -0700

    adding files

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// 'use strict';
+
 const fs = require('fs');
-// let bitmap = fs.readFileSync(__dirname + '/duck.bmp');
 var bitmap = fs.readFileSync(__dirname + '/' + process.argv[2]);
 
 
@@ -20,38 +19,27 @@ bitmapData.vip = bitmap.readUInt32LE(54);
 console.dir(bitmapData);
 console.log(bitmap.readUInt32LE(2));
 var colors ={};
-colors.getTheColors = function(){
 
-  for (var i = 54; i<64; i++){
+
+colors.getTheColors = function(){
+  for (var i = 54; i<74; i++){
     console.log('colors: ' + bitmap[i]);
   };
-  // console.log('Color: ' + bitmap[54]);
 };
-
 
 colors.alterColors = function(){
   var x = bitmap[58];
-    // var x = bitmap[90];
-  for (var i = 55; i<1078; i = i+2){
+  for (var i = 58; i<1078; i=i+3){
     bitmap[i] = x;
+    return bitmap;
   };
-
-  return bitmap;
 };
 
 colors.getTheColors();
-// colors.alterColors();
 
 colors.changeTheColors = function(){
-// 90 = green streak; // 54 = blue;// 55 = green; // 56 =red;
-
-  for(var i = 55;i<1078; i++){
-    // if i =
-    // bitmap.writeUInt32LE(255, 54);
-    bitmap.writeUInt32LE(255, i);
-    // bitmap.writeUInt32LE(0, i+1);
-    // bitmap.writeUInt32LE(0, i+2);
-
+  for(var i = 58;i<1078; i=i+3){
+    bitmap.writeUInt32LE(255, i++);
     console.log('break');
     // colors.getTheColors();
     colors.alterColors();
@@ -62,8 +50,7 @@ colors.changeTheColors = function(){
 };
 
 colors.changeTheColors();
-
-fs.writeFile('newimageXX.bmp', bitmap, (err)=>{
+fs.writeFile('adjusted_image2.bmp', bitmap, (err)=>{
   if (err) throw err;
   console.log('done');
 });
